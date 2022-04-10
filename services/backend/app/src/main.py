665daf84f1c881752b84301cce208367e22373d3
rmalhotra2192 <therishabhmalhotra@gmail.com>
2022-04-10 04:02:49 -0400

    updated main.py and ecs.yaml

--- services/backend/app/src/main.py
+++ services/backend/app/src/main.py
@@ -330,18 +330,18 @@ app.include_router(
     )
 )
 
-origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
-    "http://localhost",
-    "http://localhost:8080",
-    "http://127.0.0.1",
-    "http://127.0.0.1:8080",
-]
+# origins = [
+#     "http://localhost.tiangolo.com",
+#     "https://localhost.tiangolo.com",
+#     "http://localhost",
+#     "http://localhost:8080",
+#     "http://127.0.0.1",
+#     "http://127.0.0.1:8080",
+# ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=['*'],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
