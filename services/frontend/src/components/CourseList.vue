1217070594bb26300d79a8780a8acfba248d68e8
rmalhotra2192 <therishabhmalhotra@gmail.com>
2022-04-11 09:50:34 -0400

    fixed lint related errors

--- services/frontend/src/components/CourseList.vue
+++ services/frontend/src/components/CourseList.vue
@@ -2,10 +2,9 @@
   <div class="space-y-10 mb-10">
     <div class="grid grid-cols-12 space-x-10">
       <div class="col-span-2">
-        <router-link
-            :to="{ name: 'detail', params: { courseid: course.id } }"
-            ><img class="w-full rounded-md" :src="course.image_url" /></router-link
-          >
+        <router-link :to="{ name: 'detail', params: { courseid: course.id } }"
+          ><img class="w-full rounded-md" :src="course.image_url"
+        /></router-link>
       </div>
       <div class="col-span-10 space-y-4">
         <div class="text-3xl">
--- services/frontend/src/views/ViewerPage.vue
+++ services/frontend/src/views/ViewerPage.vue
@@ -7,20 +7,22 @@
         <div v-if="current_chapter" class="text-lg self-end">
           <router-link
             :to="{ name: 'detail', params: { courseid: $route.params.c_id } }"
-            >
+          >
             <div class="flex items-center space-x-2">
-              <div class="flex"><svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-5 w-5"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm.707-10.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L9.414 11H13a1 1 0 100-2H9.414l1.293-1.293z"
-                  clip-rule="evenodd"
-                />
-              </svg></div>
+              <div class="flex">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="h-5 w-5"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm.707-10.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L9.414 11H13a1 1 0 100-2H9.414l1.293-1.293z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </div>
               <div class="flex">{{ course.title }}</div>
             </div>
           </router-link>
